7a1f268f708564dca782971c98850d259a6bf735
cerabbite <98265679+Cerabbite@users.noreply.github.com>
2025-09-17 13:22:42 +0200

    Create basic Class based tkinter window

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from mandelbrot!")
+import tkinter as tk
+from tkinter import Label, ttk
 
+class Windows():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Test Application")
+        self.root.geometry("600x600")
+        self.root.minsize(600,600)
+        self.root.maxsize(600,600)
+
+        Label(self.root,text="Hello World").pack()
+
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    testObj = Windows()

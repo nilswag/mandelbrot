741084858657b078e8912b519c6ec99244619214
cerabbite <98265679+Cerabbite@users.noreply.github.com>
2025-09-19 10:09:20 +0200

    Working mandelbrot picture with shifting in x, y and zoom

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import colorsys
+import random
 import tkinter as tk
 from tkinter import Button, Entry, Label, StringVar, ttk
 
@@ -6,7 +8,10 @@ from PIL import Image, ImageTk
 
 from math import sqrt
 
-class App():
+import numpy as np
+
+
+class App:
     def __init__(self):
         # Initieer tkinter and geef de window een titel
         self.root = tk.Tk()
@@ -17,21 +22,21 @@ class App():
 
         Label(self.root, text="Midden x").pack()
         self.midden_x_var = StringVar()
-        Entry(self.root, textvariable = self.midden_x_var).pack()
+        Entry(self.root, textvariable=self.midden_x_var).pack()
 
-        Label(self.root, text = "Midden y").pack()
+        Label(self.root, text="Midden y").pack()
         self.midden_y_var = StringVar()
-        Entry(self.root, textvariable = self.midden_y_var).pack()
+        Entry(self.root, textvariable=self.midden_y_var).pack()
 
-        Label(self.root, text = "Schaal").pack()
+        Label(self.root, text="Schaal").pack()
         self.schaal_var = StringVar()
-        Entry(self.root, textvariable = self.schaal_var).pack()
+        Entry(self.root, textvariable=self.schaal_var).pack()
 
-        Label(self.root, text = "Max aantal").pack()
+        Label(self.root, text="Max aantal").pack()
         self.max_aantal_var = StringVar()
-        Entry(self.root, textvariable = self.max_aantal_var).pack()
+        Entry(self.root, textvariable=self.max_aantal_var).pack()
 
-        Button(self.root, text="Go", command = self.get_input_values).pack()
+        Button(self.root, text="Go", command=self.get_input_values).pack()
 
         # self.image = tk.Image(self.root)
 
@@ -40,7 +45,7 @@ class App():
         self.canvas = Label(self.root)
 
         self.root.mainloop()
-    
+
     def get_input_values(self):
         try:
 
@@ -59,29 +64,42 @@ class App():
         print(f"Schaal: {self.schaal_var.get()}")
         print(f"Max Aantal: {self.max_aantal_var.get()}")
 
-        image = Image.new(mode="RGBA", size=(500, 500))
+        WIDTH = 500
+        image = Image.new(mode="RGBA", size=(WIDTH, WIDTH))
         pixels = image.load()
 
         x = 0
         y = 0
 
-        for x in range(500):
-            for y in range(500):
-                mandel_values = self.mandelbrot(x, y, max_aantal)
+        multiplier = a  # <- Dit is de x-as verschuiving 0.5 is 0
+        divider = schaal  # <- Zoom level aka schaal 1 dan is die gecentreerd
+        verschuiving_y = b
+
+        for x in range(WIDTH):
+            for y in range(WIDTH):
+                mandel_values = self.mandelbrot(
+                    (x - (multiplier * WIDTH)) / (WIDTH / divider),
+                    ((y - (WIDTH / divider)) / (WIDTH / divider) - verschuiving_y),
+                    max_aantal,
+                )
                 pixels[x, y] = mandel_values
 
         # image.save()
         image.show()
+
     def mandelbrot(self, x, y, max_recursion):
         c = complex(x, y)
         z = 0
         for i in range(1, max_recursion):
             if abs(z) > 2:
-                # print(i)
-                return (int(i / 255), int(i / 255), int(255))
+                if i % 2 == 0:
+                    return (255, 255, 255)
+                else:
+                    return (0, 0, 0)
             z = z * z + c
-        
-        return (0,0,0)
+
+        # print("Max recursion reached")
+        return (255, 0, 255)
 
 
 if __name__ == "__main__":
